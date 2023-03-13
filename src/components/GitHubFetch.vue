18e50b0806cdad6dfd1e594e19292efed32d6188
Esther Raphael <estherraphael126@gmail.com>
2023-03-13 16:21:28 +0100

    updated

--- src/components/GitHubFetch.vue
+++ src/components/GitHubFetch.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
             <div>
-                //Displaying repositories fetched from the GitHub API
+                <!--Displaying repositories fetched from the GitHub API -->
                 <div>
                     <table>
                         <thead>
@@ -16,7 +16,7 @@
                         </thead>
 
                         <tbody>
-                            <tr v-for="repository in repositories" :key="repository.id">
+                            <tr v-for="repository in paginationHandler" :key="repository.id">
                                 <td>{{repository.id}}</td>
                                 <td>{{repository.name}}</td>
                                 <td><a :href="repository.html_url" target="_blank">{{repository.html_url}}</a></td>
@@ -29,40 +29,73 @@
             </div>
 
 
-            //Handling Pagination for the repositories
-            <ul>
-                <li v-for="page in totalPages" :key="page" @click="currentPage = page">
-                {{ page }}
-                </li>
-            </ul>
+            <!--Handling Pagination for the repositories -->
+            <div>
+                <p class="totalPages">Pages: {{ page }} of {{ totalPages }}</p>
+
+                <br />
+                <!-- button to handle the previous page -->
+                <button class="prev-next" :disabled="page <= 1" :aria-disabled="page <= 1" @click="prevPage">Prev</button>
+
+                <!-- button to handle the buttonPages displayed at a time -->
+                <button v-for="pageNumber in Array.from({ length: totalPages }, (_, index) => index + 1)"
+                        :key="pageNumber"
+                        class="usersBtn"
+                        :class="{ active: pageNumber === page }"
+                        @click="setPage(pageNumber)">
+                    {{ pageNumber }}
+                </button>
+
+                <!-- button to handle the next page -->
+                <button class="prev-next" :disabled="page >= totalPages" :aria-disabled="page >= totalPages" @click="nextPage">Next</button>
+            </div>
+           
+
         </div>
     </div>
 </template>
+
 <script>
 export default {
     data(){
         return {
             repositories: [],
-            currentPage: 1,
+            page: 1,
             perPage: 4
         }
-    },
-    computed: {
-        paginatedRepositories() {
-        const startIndex = (this.currentPage - 1) * this.pageSize;
-        const endIndex = startIndex + this.pageSize;
-        return this.repositories.slice(startIndex, endIndex);
-        },
-        totalPages() {
-        return Math.ceil(this.repositories.length / this.pageSize);
-        },
     },
     mounted(){
         fetch("https://api.github.com/users/Eceegit/repos")
         .then(response => response.json())
         .then(data => this.repositories = data)
-        .catch(error => console.log('Error Encoutered when fetching data:', error))
+        .catch(error => console.log('Error Encoutered when fetching data:', error, ))
+    },
+    computed: {
+        paginationHandler() {
+        const startIndex = (this.page - 1) * this.perPage;
+        const endIndex = startIndex + this.perPage;
+        return this.repositories.slice(startIndex, endIndex);
+        },
+        totalPages() {
+        return Math.ceil(this.repositories.length / this.perPage);
+        },
+    },
+    methods: {
+        setPage(pageNumber) {
+            this.page = pageNumber;
+        },
+        prevPage() {
+            if (this.page > 1) {
+                this.page--;
+            }
+        },
+        nextPage() {
+            if (this.page < this.totalPages) {
+                this.page++;
+            }
+        }
     }
+
 }
 </script>
 <style lang="">
